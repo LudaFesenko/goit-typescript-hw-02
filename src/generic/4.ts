67b3764f82533cab5279292feff672c65340e7c2
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2023-11-23 16:44:17 +0200

    completed generic

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentOfProps {
+  title: string;
 }
-
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponentOfProps> {
+  constructor(public props: T) {}
+}
+interface PageProps extends ComponentOfProps {
+  title: string;
+}
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
